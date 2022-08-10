8966602e6e36186d0ea493745b7bfdc5450f09c4
Anwar Sh <alvaro.sh03@gmail.com>
2022-08-10 22:03:17 +0500

    Implement rendering anime to watch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useState } from "react"
+
+const Anime = (props) => {
+  return (
+    <li>
+      {props.anime.name}
+      <button onClick={props.handleWatched}>watched</button>
+    </li>
+  )
+}
+
+const ToWatch = (props) => {
+  return (
+    <ul>
+      {props.anime.map(a => <Anime key={a.id} anime={a} handleWatched={() => props.handleWatched(a.id)}/>)}
+    </ul>
+  )
+}
+
 const App = () => {
+  const [anime, setAnime] = useState([
+    {
+      id: 1,
+      name: "anime 1",
+      link: "link 1"
+    },
+    {
+      id: 2,
+      name: "anime 2",
+      link: "link 2"
+    },
+    {
+      id: 3,
+      name: "anime 3",
+      link: "link 3"
+    }
+  ])
+
+  const handleWatched = (id) => {
+    console.log(`watched ${id}`)
+    setAnime(anime.filter(a => a.id !== id))
+  }
+
   return (
     <div>
-      watchlist
+      <h2>Anime Watchlist</h2>
+
+      <h3>anime to watch</h3>
+
+      <ToWatch anime={anime} handleWatched={handleWatched}/>
     </div>
   )
 }
