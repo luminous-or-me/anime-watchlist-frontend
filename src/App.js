52f174fc75bd82b85a522adc6ffd182d8af7e50d
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-23 13:15:36 +0500

    Implement Togglable component

--- src/App.js
+++ src/App.js
@@ -114,6 +114,35 @@ const LoginForm = (props) => {
   )
 }
 
+const Togglable = (props) => {
+  const [hidden, setHidden] = useState(true)
+
+  const visibleStyle = {
+    display: hidden ? 'none' : ''
+  }
+  const hiddenStyle = {
+    display: hidden ? '' : 'none'
+  }
+
+  return (
+    <div>
+      <div style={visibleStyle}>
+        <div>
+          {props.children}
+        </div>
+        <button onClick={() => setHidden(true)}>
+          cancel
+        </button>
+      </div>
+      <div style={hiddenStyle}>
+        <button onClick={() => setHidden(false)}>
+          {props.buttonLabel}
+        </button>
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   const [anime, setAnime] = useState([])
   const [addHidden, setAddHidden] = useState(true)
